5bffeeb62da1c927e00ad1b5bd2da6f49d78cfbe
David Carter <dajocarter@gmail.com>
2015-11-13 10:45:29 -0500

    Replace grunt-build-control with jit-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function (grunt) {
   // Show elapsed time after tasks run
   require('time-grunt')(grunt);
   // Load all Grunt tasks
-  require('load-grunt-tasks')(grunt);
+  require('jit-grunt')(grunt);
 
   grunt.initConfig({
     // Configurable paths
