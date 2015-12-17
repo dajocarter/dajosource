9cb2ac3bc57ce13cf03281f0018c00c89c0ca7c6
David Carter <dajocarter@gmail.com>
2015-12-17 16:40:18 -0500

    Style posts a bit more

--- app/_js/script.js
+++ app/_js/script.js
@@ -15655,49 +15655,6 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
     }
   };
 })(jQuery);
-;/*global jQuery */
-/*!
-* FitText.js 1.2
-*
-* Copyright 2011, Dave Rupert http://daverupert.com
-* Released under the WTFPL license
-* http://sam.zoy.org/wtfpl/
-*
-* Date: Thu May 05 14:23:00 2011 -0600
-*/
-
-(function( $ ){
-
-  $.fn.fitText = function( kompressor, options ) {
-
-    // Setup options
-    var compressor = kompressor || 1,
-        settings = $.extend({
-          'minFontSize' : Number.NEGATIVE_INFINITY,
-          'maxFontSize' : Number.POSITIVE_INFINITY
-        }, options);
-
-    return this.each(function(){
-
-      // Store the object
-      var $this = $(this);
-
-      // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
-
-      // Call once to set.
-      resizer();
-
-      // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-    });
-
-  };
-
-})( jQuery );
 ;/*
  *  /MathJax.js
  *
@@ -15883,10 +15840,10 @@ jQuery(document).ready(function($) {
     var y = $(window).scrollTop();
     var nav = $('#nav-wrap');
 
-    if ((y > h * .20) && (y < h) && ($(window).outerWidth() > 768)) {
+    if ((y > h * 0.20) && (y < h) && ($(window).outerWidth() > 768)) {
       nav.fadeOut('fast');
     } else {
-      if (y < h * .20) {
+      if (y < h * 0.20) {
         nav.removeClass('opaque').fadeIn('fast');
       } else {
         nav.addClass('opaque').fadeIn('fast');
